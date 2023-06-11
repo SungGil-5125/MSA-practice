df99788614bc24f3b6ef7bee9f4c194ebbc81aa6
SungGil-5125 <82089918+SungGil-5125@users.noreply.github.com>
2023-06-11 15:44:15 +0900

    :sparkles: :: rabbitMQ properties 추가

---
+++ publisher/src/main/kotlin/com/project/publisher/property/RabbitMQProperty.kt
@@ -0,0 +1,9 @@
+package com.project.publisher.property
+
+object RabbitMQProperty {
+
+    const val EXCHANGE_NAME = "test.exchange"
+    const val QUEUE_NAME = "test.queue"
+    const val ROUTING_KEY = "test.starload.#"
+
+}
